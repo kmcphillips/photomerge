53f089a695a8d3a0afad6fddf95e357a222f2607
Kevin McPhillips <github@kevinmcphillips.ca>
2015-12-07 10:04:49 -0800

    Use active support

--- photomerge.rb
+++ photomerge.rb
@@ -1,9 +1,10 @@
 require "rubygems"
 require "bundler/setup"
+
+require "active_support/all"
 require "fileutils"
 require "pry"
 
 Dir["./lib/*.rb"].each{ |f| require f }
 
 class PhotoMergeError < StandardError ; end
-
