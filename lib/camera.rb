0c92f231d841648d794708b2cd63c20b6e4189a9
Kevin McPhillips <github@kevinmcphillips.ca>
2015-12-18 14:57:01 -0500

    Add other cameras. output. suffix

--- lib/camera.rb
+++ lib/camera.rb
@@ -1,16 +1,37 @@
 class Camera
-  attr_reader :name, :folder
+  attr_reader :name, :folder, :suffix
 
-  def initialize(path, name: nil)
+  def initialize(path, name: nil, suffix: nil, output: nil)
+    @folder = validated_folder(path)
+
+    if output.present?
+      @output = validated_folder(output)
+    else
+      @output = validated_folder(File.join(path, "output"))
+    end
+
+    @name = name.presence || @folder.path.split("/").last
+    @suffix = suffix.presence || @name
+
+    load_photos
+  end
+
+  private
+
+  def load_photos
+    @photos = []
+
+
+  end
+
+  def validated_folder(path)
     if File.exists?(path)
-      @folder = File.new(path)
-      raise PhotoMergeError, "Path #{ path } is not a directory" unless File.directory?(@folder)
+      folder = File.new(path)
+      raise PhotoMergeError, "Path #{ path } is not a directory" unless File.directory?(folder)
     else
       raise PhotoMergeError, "Path #{ path } does not exist"
     end
 
-    @name = name.presence || @folder.path
+    folder
   end
-
-
 end
--- photomerge.rb
+++ photomerge.rb
@@ -8,3 +8,30 @@ require "pry"
 Dir["./lib/*.rb"].each{ |f| require f }
 
 class PhotoMergeError < StandardError ; end
+
+output = "/home/kevin/Desktop/Brocember/output"
+
+cameras = [
+  Camera.new(
+    "/home/kevin/Desktop/Brocember/kevin",
+    output: output,
+    name: "kevin",
+    suffix: "km",
+  ),
+
+  Camera.new(
+    "/home/kevin/Desktop/Brocember/patrick",
+    output: output,
+    name: "patrick",
+    suffix: "ph",
+  ),
+
+  Camera.new(
+    "/home/kevin/Desktop/Brocember/eliot",
+    output: output,
+    name: "eliot",
+    suffix: "eb",
+  ),
+]
+
+binding.pry
